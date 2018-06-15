4024e28e8070a64eafe561244c3e2d2de75708d0
smurphin <email.darrenmurphy@gmail.com>
2018-06-15 17:24:52 +0100

    added exercises

---
+++ ex2.py
@@ -0,0 +1,12 @@
+#These are just some comments
+# To show what the # can do
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.") # even if I put it after the command it will be fine
+
+#just another comments
+print("This is fun.")
+print('Yay! Printing')
+print("I'd much rather you 'not'!")
+print('I "said" do not touch this.')

e871a698cef60448fdbc244c7d8e2e102ba6817f
Murphy Darren <darren.murphy@nagra.com>
2018-07-19 16:20:18 +0100

    updated typo in ex2

--- ex2.py
+++ ex2.py
@@ -5,7 +5,7 @@ print("Hello World!")
 print("Hello Again")
 print("I like typing this.") # even if I put it after the command it will be fine
 
-#just another comments
+# just another comment
 print("This is fun.")
 print('Yay! Printing')
 print("I'd much rather you 'not'!")

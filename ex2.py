6039c98d182ca19112237911d34ff6c5e0510d47
smurphin <email.darrenmurphy@gmail.com>
2018-08-20 16:28:32 +0100

    added ex3.py

--- ex2.py
+++ ex2.py
@@ -1,12 +1,13 @@
-#These are just some comments
+# These are just some comments
 # To show what the # can do
 
 print("Hello World!")
 print("Hello Again")
 print("I like typing this.") # even if I put it after the command it will be fine
 
-# just another comment
+# just another comments
 print("This is fun.")
 print('Yay! Printing')
 print("I'd much rather you 'not'!")
 print('I "said" do not touch this.')
+# test comment
